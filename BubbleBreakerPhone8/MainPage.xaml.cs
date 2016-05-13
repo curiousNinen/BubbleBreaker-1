d0fceb0b920da44af1b2ac6ef25a9e016649fb44
Roger S. <roger.spiess@web.de>
2016-05-13 04:15:24 +0200

    Windows Phone 8.1 Version hinzugefügt

--- BubbleBreakerPhone8/MainPage.xaml.cs
+++ BubbleBreakerPhone8/MainPage.xaml.cs
@@ -1,14 +1,12 @@
-﻿using System;
+﻿using BubbleBreakerLib;
+using BubbleBreakerUWP;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
-using System.Threading;
-using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI;
-using Windows.UI.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -16,12 +14,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Windows.UI.Xaml.Shapes;
-using BubbleBreakerLib;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
 
-namespace BubbleBreakerUWP
+namespace BubbleBreakerPhone8
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -36,6 +32,24 @@ namespace BubbleBreakerUWP
         {
             this.InitializeComponent();
             Highscore = 0;
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
         }
 
         /// <summary>
