c2d2dcee3f7a345f87ee1b6632c2c291e2e9b3fc
Roger S. <roger.spiess@web.de>
2016-05-14 00:15:53 +0200

    Aufräumen 3

--- BubbleBreakerLib/Properties/AssemblyInfo.cs
+++ BubbleBreakerLib/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BubbleBreakerPCL")]
+[assembly: AssemblyTitle("BubbleBreakerLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BubbleBreakerPCL")]
+[assembly: AssemblyProduct("BubbleBreakerLib")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
